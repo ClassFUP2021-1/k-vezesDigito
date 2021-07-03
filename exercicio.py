0eb1c3d1e8838d0767c1bb58e3eb9c2ee0d659e8
fupufc <86124269+fupufc@users.noreply.github.com>
2021-07-03 18:18:30 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,2 +1,28 @@
 def resposta():
   pass
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+cont = 0 
+cont_num2 = 0
+num1_valido = True
+
+while True:
+   
+    if num1 < 0 or num1 > 9:
+        num1_valido = False
+        break        
+    else:      
+        unidade = num2 % 10          
+        if num1 == unidade:
+            cont = cont + 1     
+        
+        num2 = num2//10 
+        cont_num2 = cont_num2 + 1  
+
+
+if  num1_valido== True or (cont_num2 == 8 or cont_num2 == 9):
+    print(cont)
+elif (num1_valido == False) or (cont_num2 != 8) or (cont_num2 != 9):
+    print('entrada invalida')
